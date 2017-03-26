d0983d8159ddb96ef9dab20de216b32e3e61bef2
Jue <me@jue.yt>
2017-03-26 23:43:23 +0200

    Made sure exisiting databases from other apps are rejected

--- test/00-database.js
+++ test/00-database.js
@@ -1,16 +1,54 @@
 'use strict';
 
 const assert = require( 'assert' );
+const fs = require( 'fs' );
 
 describe( "database", function() {
 
 	const dbFactory = require( '../lib/database.js' );
+
+	// Creates a tmp dir for local data
+	let tmpdir;
+	before( () => {
+		tmpdir = fs.mkdtempSync( '/tmp/cashy-test-' );
+	} );
+	after( () => {
+		deleteFolderRecursive( tmpdir );
+		function deleteFolderRecursive( path ) {
+			if( fs.existsSync( path ) ) {
+				fs.readdirSync( path ).forEach( ( file, index ) => {
+					const curPath = path + "/" + file;
+					if( fs.lstatSync( curPath ).isDirectory() ) {
+						deleteFolderRecursive( curPath );
+					} else {
+						fs.unlinkSync( curPath );
+					}
+				} );
+				fs.rmdirSync(path);
+			}
+		};
+	} );
 
 	it( "should create new database", ( done ) => {
 		dbFactory( ':memory:' ).then( ( db ) => {
 			return db.get( 'PRAGMA application_id;' );
 		} ).then( ( row ) => {
 			assert.strictEqual( row.application_id, 0x1337beef );
+			done();
+		} ).catch( done );
+	} );
+
+	it( "should reject existing databases with wrong app id and tables inside", ( done ) => {
+		const DB = require( '../lib/sqlite.js' );
+		const db = new DB( `${tmpdir}/nonempty.sqlite` );
+		db.run( 'CREATE TABLE test(val);' ).then( ( ) => {
+			return db.close();
+		} ).then( () => {
+			return dbFactory( `${tmpdir}/nonempty.sqlite` );
+		} ).then( () => {
+			done( new Error( "Did not throw an exception" ) );
+		} ).catch( (e) => {
+			assert.strictEqual( e.message, "Not a valid database" );
 			done();
 		} ).catch( done );
 	} );
