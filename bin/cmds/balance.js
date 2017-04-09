cee8f590c5378946c749b149827917095ded2619
Jue <me@jue.yt>
2017-04-10 01:54:34 +0200

    Extended CLI: Balance output

---
+++ bin/cmds/balance.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const Cashy = require( '../../index.js' );
+const Table = require( '../lib/table.js' );
+const format = require( '../lib/format.js' );
+
+
+// Output table
+const out = new Table( process.stdout );
+
+
+module.exports = (program) => program
+	.command( 'balance' )
+	.option( '-d --date <date>', "balances at this date" )
+	.option( '--csv', "output comma-separated account list" )
+	.description( "list all accounts including their balances" )
+	.action( balance );
+
+function balance( opts ) {
+
+	let filter = {};
+	if( typeof opts.date == 'string' ) {
+		filter.date = (opts.date == 'now') ? new Date() : new Date( opts.date );
+	}
+
+	let cashy = Cashy( {
+		create: false,
+		file: opts.parent.file
+	} );
+
+	cashy.getAccounts( filter ).then( (accounts) => {
+
+		let jobs = [];
+		for( let a of accounts ) jobs.push( a.balance( filter ) );
+		return Promise.all( jobs ).then( (balances) => [accounts,balances] );
+
+	} ).then( (args) => {
+
+		const accounts = args[0];
+		const balances = args[1];
+
+		if( opts.csv ) {
+
+			for( let a in accounts ) {
+				console.log( `${accounts[a].id},${balances[a]}` );
+			}
+
+		} else {
+
+			// Heading
+			out.write( "Account", { pos: 1 } );
+			out.write( "Balance", { pos: -1, align: 'right' } );
+			out.nl();
+			out.line( 'blackBright' );
+			// Bidy
+			for( let a in accounts ) {
+				let path = accounts[a].id.split( '/' );
+				let pos = 1 + 2 * ( path.length - 1 );
+				let caption = path.pop();
+				out.write( caption, {
+					pos: pos,
+					color: accounts[a].dateClosed ? 'blackBright' : 'white'
+				} );
+				out.write( format( balances[a], cashy.accuracy ), {
+					pos: -1,
+					align: 'right',
+					color: (balances[a] >= 0) ? 'green' : 'red'
+				} );
+				out.nl();
+			}
+
+		}
+
+	} ).catch( ( e ) => {
+		console.error( e.message );
+		process.exit( 1 );
+	} );
+
+}
---
+++ bin/lib/format.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function( number, n, x ) {
+	const re = new RegExp( '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')', 'g' );
+	return number.toFixed( Math.max( 0, ~~n ) ).replace( re , '$&,' );
+};
